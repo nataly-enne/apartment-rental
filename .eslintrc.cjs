fd5dbb3e658a42c935488fcb8efabcff5a718636
nataly-enne <ennenataly@gmail.com>
2023-07-10 20:21:41 -0300

    ci: install styled-components and ci configurations

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -23,6 +23,10 @@ module.exports = {
       { allowConstantExport: true },
     ],
     '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-unsafe-assignment': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unsafe-call': 'off',
+    '@typescript-eslint/no-unsafe-member-access': 'off',
   },
   ignorePatterns: [".eslintrc.cjs", "vite.config.ts"],
 }
