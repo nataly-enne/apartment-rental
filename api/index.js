b31952f9a853936030cf88c72b564dc12bb000ae
nataly-enne <ennenataly@gmail.com>
2023-07-20 01:09:47 -0300

    implementing api to filter by URL query params

--- api/index.js
+++ api/index.js
@@ -4,7 +4,29 @@ const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-server.use(jsonServer.bodyParser);
+
+// converte somente a primeira letra para maiúscula
+const toLowerCaseFirstLetter = (value) => (value ? value.toLowerCase() : value);
+
+// filtra os imóveis com base na cidade e tipo (ignorando diferença de maiúsculas e minúsculas)
+server.get("/properties", (req, res) => {
+  const city = toLowerCaseFirstLetter(req.query.city) ;
+  const type = toLowerCaseFirstLetter(req.query.type) ;
+
+  let properties = router.db.get("properties").value();
+
+  if (city || type) {
+    if (city) {
+      properties = properties.filter((property) => toLowerCaseFirstLetter(property.city) === city);
+    }
+    if (type) {
+      properties = properties.filter((property) => toLowerCaseFirstLetter(property.type) === type);
+    }
+  }
+
+  res.json(properties);
+});
+
 server.use(router);
 
 server.listen(5099, () => {
